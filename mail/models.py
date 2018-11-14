3828979bdeb3e02596454b99f0749b7e4293a8f5
jmpark6846 <jmpark6846@naver.com>
2018-11-14 18:51:51 +0900

    first commit

---
+++ mail/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+import datetime
+# Create your models here.
+
+class Recruit(models.Model):
+    company = models.CharField('회사명', max_length=255)
+    field = models.CharField('모집 분야', max_length=255)
+    duedate = models.DateField('마감일', blank=True, null=True)
+    place = models.CharField('근무지', max_length=255)
+    career = models.CharField('경력 구분', max_length=255)
+    detail = models.CharField('특이사항', max_length=255)
+
+    def due(self):
+        if not self.duedate:
+            return 'ASAP'
+        return datetime.datetime.strptime(self.duedate, '~m/d')
+
+    def __str__(self):
+        return '{} / {} / {}'.format(self.company, self.field, self.duedate)
+
